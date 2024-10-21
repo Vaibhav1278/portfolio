d631f011f4d2505b62bf9a71d76bc62ba09e1f86
102vansh <135614239+102vansh@users.noreply.github.com>
2024-10-21 14:12:50 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -345,7 +345,7 @@ export const projects = [
       "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
     // image:
     //   "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
-      video:"https://drive.google.com/file/d/1bQCX9C8VS9chWHwJdGHa9--jvPzsOwxh/preview",
+      video:"https://drive.google.com/file/d/1Ml-2g-G5_dSlszXe1nXGJbDbAEl_oVwt/preview",
       
 
     tags: [
